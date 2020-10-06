4d8748dd21885e444b57b716abbf84277e215196
Frost <magomedns@gmail.com>
2020-10-06 18:10:56 +0300

    add json 2

--- App.tsx
+++ App.tsx
@@ -33,7 +33,81 @@ const VainahStream =
  
   // Путь https://live.chechensoft.ru/tvput/tvput/playlist.m3u8
 
- 
+  
+const DATA = [
+  {
+    key: "1",
+    title: "ЧГТРК Грозный",
+    icon: require("./assets/images/grozny-icon.png"),
+    url: "Grozny",
+  },
+  {
+    key: "2",
+    title: "ТРК Путь",
+    icon: require("./assets/images/put-icon.png"),
+    url: "Put",
+  },
+  {
+    key: "3",
+    title: "ТРК Вайнах",
+    icon: require("./assets/images/vainah-icon.png"),
+    url: "Vainah",
+  },
+  {
+    key: "4",
+    title: "Карусель",
+    icon: require("./assets/images/1tv.png"),
+    url: "Grozny",
+  },
+  {
+    key: "5",
+    title: "ТРК Путь",
+    icon: require("./assets/images/1tv.png"),
+    url: "Put",
+  },
+  {
+    key: "6",
+    title: "ТРК Вайнах",
+    icon: require("./assets/images/1tv.png"),
+    url: "Vainah",
+  },
+  {
+    key: "7",
+    title: "ЧГТРК Грозный",
+    icon: require("./assets/images/grozny-icon.png"),
+    url: "Grozny",
+  },
+  {
+    key: "8",
+    title: "ТРК Путь",
+    icon: require("./assets/images/put-icon.png"),
+    url: "Put",
+  },
+  {
+    key: "9",
+    title: "ТРК Вайнах",
+    icon: require("./assets/images/vainah-icon.png"),
+    url: "Vainah",
+  },
+  {
+    key: "10",
+    title: "ЧГТРК Грозный",
+    icon: require("./assets/images/grozny-icon.png"),
+    url: "Grozny",
+  },
+  {
+    key: "11",
+    title: "ТРК Путь",
+    icon: require("./assets/images/put-icon.png"),
+    url: "Put",
+  },
+  {
+    key: "12",
+    title: "ТРК Вайнах",
+    icon: require("./assets/images/vainah-icon.png"),
+    url: "Vainah",
+  },
+];
 
 const MyTheme = {
   dark: false,
