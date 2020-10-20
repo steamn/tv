fd6395ef6456fda9a028df623e022bdf0daddf75
Frost <magomedns@gmail.com>
2020-10-20 18:06:25 +0300

    add channels

--- src/streams.tsx
+++ src/streams.tsx
@@ -1,12 +1,19 @@
  
 export const Streams = {
     GroznyStream: "https://edge2-tv-ll.facecast.io/evacoder_hls_hi/UUMLQVAYVlZyH14GRENQVV0G/2/720-3.m3u8",
-    PutStream: "http://dmitry-tv.my1.ru/his/02/CH_NATGEOHD.m3u8",
+    PutStream: "https://live.chechensoft.ru/tvput/tvput/playlist.m3u8",
     VainahStream: "https://live.chechensoft.ru/vainahtv/ngrp:vaynahtv_all/playlist.m3u8",
+    tv1: "http://dmitry-tv.my1.ru/his/01/CH_1TV.m3u8",
+    russia1: "http://dmitry-tv.my1.ru/his/01/CH_RUSSIA1.m3u8",
+    kultura: "http://dmitry-tv.my1.ru/his/01/CH_RUSSIAK.m3u8",
+    russia24: "http://dmitry-tv.my1.ru/his/01/CH_RUSSIA24.m3u8",
+    tvc: "http://dmitry-tv.my1.ru/his/02/CH_TVC.m3u8",
+    rentv: "http://dmitry-tv.my1.ru/his/01/CH_RENTV.m3u8",
+    zvezda: "http://dmitry-tv.my1.ru/his/02/CH_ZVEZDA.m3u8",
+    mir: "http://dmitry-tv.my1.ru/his/01/CH_MIRHD.m3u8",
+    ufc: "http://dmitry-tv.my1.ru/his/02/CH_UFCHD.m3u8",
+    match: "http://dmitry-tv.my1.ru/his/01/CH_MATCHTV.m3u8",
 }
  
 
-
- 
-
-// Путь https://live.chechensoft.ru/tvput/tvput/playlist.m3u8
+// http://live-ng-01.more.tv/hls/UFC/index_6.m3u8
